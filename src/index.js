5d2462be735e4b75db93cca78b7c09b42a57548d
Justin Mastic <jmastic@gmail.com>
2017-10-01 10:09:55 -0600

    Adding yarn and hot module reloader

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+const render = (Component) =>
+  ReactDOM.render(
+    <App>
+      <Component />
+    </App>,
+    rootEl
+  );
+
+render(App);
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => render(App));
+}
