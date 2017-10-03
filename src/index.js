9b19968920e04d3b3991d68903a8c0d74ae51448
Justin Mastic <jmastic@gmail.com>
2017-10-02 21:01:19 -0600

    Saving progress on controls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
+require('dotenv').config();
 
 const rootEl = document.getElementById('root');
 const render = (Component) =>
@@ -17,5 +18,5 @@ render(App);
 registerServiceWorker();
 
 if (module.hot) {
-  module.hot.accept('./App', () => render(App));
+  module.hot.accept('./containers/App/App', () => render(App));
 }
