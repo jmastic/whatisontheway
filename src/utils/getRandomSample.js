1287f86d91c1ac90ac056734b603b85864fb32b4
Justin Mastic <jmastic@gmail.com>
2022-07-31 16:19:31 -0600

    Add unit tests for utils

--- src/utils/getRandomSample.js
+++ src/utils/getRandomSample.js
@@ -1,4 +1,7 @@
-export default function getRandomSample(arr, num) {
-  const shuffled = [...arr].sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, num);
+export default function getRandomSample(inArray, sampleSize) {
+  if (!inArray) {
+    return null;
+  }
+  const shuffled = [...inArray].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, sampleSize);
 }
--- src/utils/rangePercentage.js
+++ src/utils/rangePercentage.js
@@ -1,3 +1,5 @@
+// Will take in a range and a percent
+// Returns the number at the percentage within the range
 export function getNumberAtRangePercent(percentage, max, min) {
   let num = percentage * (max - min) + min;
   if (num > max) {
@@ -8,6 +10,8 @@ export function getNumberAtRangePercent(percentage, max, min) {
   return num;
 }
 
+// Will take in a range and a number
+// Returns the relative percentage of the input number within the input range
 export function getPercentageInRange(input, max, min) {
   let percentage = (input - min) / (max - min);
   if (percentage > 1) {
